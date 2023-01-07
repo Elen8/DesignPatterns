f52d9a6ce871c9826edee737c0f796aa37b3d5b9
Elen8 <90869008+Elen8@users.noreply.github.com>
2023-01-07 15:34:15 +0400

    Update adopter.cpp

--- Adopter/adopter.cpp
+++ Adopter/adopter.cpp
@@ -8,28 +8,33 @@ private:
 	long long CardNumber;
 
 public:
-	Bank()
-	{
-		BankName = "InecoBank";
-		CardHolder = "Elen Tonoyan";
-		CardNumber = 4576475829182312;
-	}
-
-	std::string get_holder()
-	{
-		return CardHolder;
-	}
-
-	std::string get_bank()
-	{
-		return BankName;
-	}
-
-	long long get_number()
-	{
-		return CardNumber;
-	}
+	Bank();
+	std::string get_holder();
+	std::string get_bank();
+	long long get_number();
 };
+
+Bank::Bank()
+{
+	BankName = "InecoBank";
+	CardHolder = "Elen Tonoyan";
+	CardNumber = 4576475829182312;
+}
+	
+std::string Bank::get_holder()
+{
+	return CardHolder;
+}
+
+std::string Bank::get_bank()
+{
+	return BankName;
+}
+
+long long Bank::get_number()
+{
+	return CardNumber;
+}
 
 class CreditCard
 {
@@ -43,15 +48,17 @@ private:
 	Bank* BankDetails;
 
 public:
-	void get_bank_details() override
-	{
-		BankDetails = new Bank();
-		std::cout << "Bank Details" << std::endl;
-		std::cout << "Bank Name: " << BankDetails->get_bank() << std::endl;
-		std::cout << "Card Holder Name: " << BankDetails->get_holder() << std::endl;
-		std::cout << "Credit Card: " << BankDetails->get_number() << std::endl;
-	}
+	void get_bank_details() override;
 };
+
+void Customer::get_bank_details() 
+{
+	BankDetails = new Bank();
+	std::cout << "Bank Details" << std::endl;
+	std::cout << "Bank Name: " << BankDetails->get_bank() << std::endl;
+	std::cout << "Card Holder Name: " << BankDetails->get_holder() << std::endl;
+	std::cout << "Credit Card: " << BankDetails->get_number() << std::endl;
+}
 
 int main()
 {
